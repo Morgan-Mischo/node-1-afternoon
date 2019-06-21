32a99634fd8a606d381b4070b134ecd922d5b250
Morgan-Mischo <goramorgan@gmail.com>
2019-06-21 15:30:40 -0600

    finished

--- server/getProduct.js
+++ server/getProduct.js
@@ -2,7 +2,7 @@ const products = require('../products.json');
 
 const getProduct = (req, res) => {
     // find returns the item if it finds it, or undefined if not
-    const item = products.find(val => val.id === parseInt(req.params.id));
+    const item = products.find(val => val.id === +req.params.id);
     if (!item) {
         return res.status(500).send("Item not in list");
     }
--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const getProducts = require('./getProducts');
-const getProduct = require('./getProduct');
+const express = require('express')
+const getProducts = require('./getProducts')
+const getProduct = require('./getProduct')
 
-const app = express();
+const app = express()
 
-const port = 4000;
+const port = 4000
 
-app.get('/api/products', getProducts);
-app.get('/api/product/:id', getProduct);
+
+app.get('/api/products', getProducts)
+app.get('/api/product/:id', getProduct)
 
 app.listen(port, () => {
-    console.log(`Server listening on port: ${port}`);
-});
+    console.log(`Welcome to your server... Please don't kill me`)
+})
